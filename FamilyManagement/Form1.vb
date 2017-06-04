2d4683d640c790397eb27c15713079604480c964
clevercoo1 <462960285@qq.com>
2017-06-04 20:11:32 +0800

    PLSQL连接测试

--- FamilyManagement/Form1.vb
+++ FamilyManagement/Form1.vb
@@ -1,22 +1,30 @@
 ﻿Imports Npgsql
 
 Public Class Form1
-
 #Region "Test"
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cn As New NpgsqlConnection
+        cn.ConnectionString = "Server=127.0.0.1;Port=5432;Database=shop;Username=postgres;Password=a789889;" +
+                              "Persist Security Info=True;CommandTimeout=3000;"
         Try
-            Dim conn = New NpgsqlConnection("Host=localhost;Username=postgres;Password=a789889;Database=shop;CommandTimeout=50;Integrated Security=true")
-            Dim strPLSql As String = String.Empty
+
+            cn.Open()
+            Dim strSQL As String
+            strSQL = "SELECT * FROM shohin where shohin_id = '0001'"
             Dim ds As New DataSet
-            Dim da As NpgsqlDataAdapter
-            da = New NpgsqlDataAdapter("SELECT * FROM shohin where shohin_id = '0001'", conn)
-            da.Fill(ds)
-            TextBox1.Text = ds.Tables(0).Rows(0).Item(0).ToString
+            Dim da As New NpgsqlDataAdapter(strSQL, cn)
+
+            da.Fill(ds, "a")
+            cn.Dispose()
+            TextBox1.Text = ds.Tables("a").Rows(0).ToString
         Catch ex As Exception
             MsgBox(ex.Message)
 
         End Try
     End Sub
+
+
 #End Region
 
 End Class
