2ac0e796ddc2ee76e61f5c665750121504c2984a
clevercoo1 <462960285@qq.com>
2017-06-04 18:16:24 +0800

    PLSQL连接测试

--- FamilyManagement/Form1.vb
+++ FamilyManagement/Form1.vb
@@ -1,3 +1,22 @@
-﻿Public Class Form1
+﻿Imports Npgsql
+
+Public Class Form1
+
+#Region "Test"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim conn = New NpgsqlConnection("Host=localhost;Username=postgres;Password=a789889;Database=shop;CommandTimeout=50;Integrated Security=true")
+            Dim strPLSql As String = String.Empty
+            Dim ds As New DataSet
+            Dim da As NpgsqlDataAdapter
+            da = New NpgsqlDataAdapter("SELECT * FROM shohin where shohin_id = '0001'", conn)
+            da.Fill(ds)
+            TextBox1.Text = ds.Tables(0).Rows(0).Item(0).ToString
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+    End Sub
+#End Region
 
 End Class
